1a0afbf040fd4366601cb84924169b241f021544
theway <liiiv97@outlook.com>
2022-11-11 17:57:00 +0800

    update loop

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,7 +1,9 @@
+extends Node2D
 enum GameState {
 	CREATING,
 	PLAYING,
 	DROPING,
+	DESTROYING,
 }
 
 const UNIT_SIZE = 32
@@ -9,11 +11,12 @@ const MAX_X = 7
 const MAX_Y = 7
 var Block = preload("res://prefabs//Block.tscn")
 var blocks = []
-var running_blocks = []
+var running_block_group = null
 var go_dir  = false
 var state = GameState.DROPING
 
 func _ready():
+	running_block_group = RunningBlockGroup.new(self, Block)
 	state = GameState.CREATING
 	add_block(2, 7)
 	add_block(3, 7)
@@ -24,19 +27,24 @@ func _ready():
 	add_block(3, 5)
 
 
-# func _process(delta):
-# 	_process_input()
-# 	if state == GameState.CREATING:
-# 		creating()
-# 	elif state == GameState.PLAYING:
-# 		droping(delta)
+func _process(delta):
+	_process_input()
+	if state == GameState.CREATING:
+		creating()
+	elif state == GameState.PLAYING:
+		playing(delta)
+	elif state == GameState.DESTROYING:
+		destroying()
 
-# func create_block(x, y = 0):
-# 	var block = Block.instance()
-# 	block.init(x, y)
-# 	add_child(block)
-# 	running_blocks.append(block)
 
+func creating():
+	running_block_group.create()
+	state = GameState.PLAYING
+
+func destroying():
+	var stoped_blocks = running_block_group.destroying()
+	blocks.append_array(stoped_blocks)
+	state = GameState.CREATING
 
 func add_block(x, y):
 	var block = Block.instance()
@@ -44,12 +52,10 @@ func add_block(x, y):
 	add_child(block)
 	blocks.append(block)
 
-
-# func creating():
-# 	create_block(4)
-# 	create_block(5)
-# 	state = GameState.PLAYING
-
+func playing(delta):
+	var is_all_stop = running_block_group.step(delta)
+	if is_all_stop:
+		state = GameState.DESTROYING
 
 # func droping(delta):
 # 	var alldone = true
@@ -57,7 +63,6 @@ func add_block(x, y):
 # 	if go_dir != 0:
 # 		for block in running_blocks:
 # 				block.hor_move(go_dir)
-
 # 	for block in running_blocks:
 # 		if go_dir != 0:
 # 			block.hor_move(go_dir)
@@ -77,23 +82,23 @@ func add_block(x, y):
 # 		state = GameState.CREATING
 
 
-# func _process_input():
-# 	go_dir = 0
-# 	if Input.is_action_pressed("ui_right"):
-# 		go_dir = 1
-# 	elif Input.is_action_pressed("ui_left"):
-# 		go_dir = -1
+func _process_input():
+	go_dir = 0
+	if Input.is_action_pressed("ui_right"):
+		go_dir = 1
+	elif Input.is_action_pressed("ui_left"):
+		go_dir = -1
 
 
 # func dis_running_blocks():
 # 	blocks.append_array(running_blocks)
 # 	running_blocks = []
 
-# func check(x, y):
-# 	if x < 0 || x > MAX_X || y < 0 || y > MAX_Y:
-# 		return false
+func check(x, y):
+	if x < 0 || x > MAX_X || y < 0 || y > MAX_Y:
+		return false
 
-# 	for block in blocks:
-# 		if block.step_x == x and block.step_y == y:
-# 			return block
-# 	return null
+	for block in blocks:
+		if block.step_x == x and block.step_y == y:
+			return block
+	return null
--- scripts/running_block_group.gd
+++ scripts/running_block_group.gd
@@ -1,31 +1,55 @@
 class_name RunningBlockGroup
 
-var Block = preload("res://Block.tscn")
 var parent : Node2D = null
 var left_block : Block = null
 var right_block : Block = null
+var block_res = null
+var blocks = [] setget , get_blocks
 
-func _init(parent):
-  parent = parent
-  pass
+func get_blocks():
+	return [left_block, right_block]
 
+func _init(_parent, _block_res):
+	parent = _parent
+	block_res = _block_res
 
 func create():
-  _generate_running() 
+	_generate_running()
 
 
-func step():
+func step(delta):
+	var allstop = true
+	# for block in blocks:
+	# 	if go_dir != 0:
+	# 		block.hor_move(go_dir)
+	# 	var done = block.step(delta)
+	# 	if done == false:
+	# 		alldone = false
+
+	for block in blocks:
+		if block.to_target():
+			allstop = false
+		block.step(delta)
+		
+		return allstop
+
+
+func free_blocks():
+	var cs = blocks
+	left_block = null
+	right_block = null
+	return cs
 
 func _generate_running():
-  left_block = _create_block(4, 0)
-  right_block = _create_block(4, 0)
+	left_block = _create_block(4, 0)
+	right_block = _create_block(5, 0)
 
 
 func _create_block(x, y):
-  var block = Block.instance() 
-  block.init(x, y)
-  parent.add_child(block)
-  return block
+	var block = block_res.instance()
+	block.init(x, y)
+	parent.add_child(block)
+	return block
 
 
 
